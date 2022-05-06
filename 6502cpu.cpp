a1c4ac1c4f17dee5aaac1e0c88147055f709a7bb
Akshay <akshay8055.007@gmail.com>
2022-05-07 00:25:23 +0100

    addressing modes and some cpu implementation

--- 6502cpu.cpp
+++ 6502cpu.cpp
@@ -268,6 +268,16 @@ cpu::cpu() {
 cpu::~cpu() {
 }
 
+void cpu::reset() {
+    //TODO
+}
+void cpu::interrupt_request() {
+    //TODO
+}
+void cpu::non_mask_interrupt() {
+    //TODO
+}
+
 void cpu::cpu_clock() {
     
     if(cycles == 0) {
@@ -502,13 +512,13 @@ void cpu::cpu_clock() {
         }
         cycles += (addition_cycle1 & addition_cycle2);
         set_flag(Unused, true);
-
+        clock_count++;
     }
     cycles--;
 
 }
 
-//--------------------------------ADDRESSING MODES----------------------------------//
+//----------------------------------------ADDRESSING MODES----------------------------------------//
 uint8_t cpu::IMP() {
     //no additional data required
     //set the fetched value to value from the accumulator
@@ -516,163 +526,419 @@ uint8_t cpu::IMP() {
     return 0;
 }
 uint8_t cpu::IMM() {
+    // The instruction expects the next byte to be used as a value, so we'll prep
+    // the read address to point to the next byte
     addr_abs = pc++;
     return 0;
-    //TODO:
 }
 uint8_t cpu::ZP0() {
+    //operand is zeropage address (hi-byte is zero, address = $00LL)
+    addr_abs = cpu_bus_read(pc);	
+	pc++;
+	addr_abs &= 0x00FF;
     return 0;
-    //TODO:
 }
 uint8_t cpu::ZPX() {
+    //operand is zeropage address; effective address is address incremented by X without carry
+    addr_abs = cpu_bus_read(pc + x_reg);	
+	pc++;
+	addr_abs &= 0x00FF;
     return 0;
-    //TODO:
 }
 uint8_t cpu::ZPY() {
+    //operand is zeropage address; effective address is address incremented by Y without carry
+    addr_abs = cpu_bus_read(pc + y_reg);	
+	pc++;
+	addr_abs &= 0x00FF;
     return 0;
-    //TODO:
 }
 uint8_t cpu::REL() {
+    //branch target is PC + signed offset BB
+    addr_rel = cpu_bus_read(pc);
+	pc++;
+	if (addr_rel & 0x80)
+		addr_rel |= 0xFF00;
     return 0;
-    //TODO:
 }
 uint8_t cpu::ABS() {
+    //full 16 bit address
+    uint16_t lo = cpu_bus_read(pc);
+	pc++;
+	uint16_t hi = cpu_bus_read(pc);
+	pc++;
+	addr_abs = (hi << 8) | lo;
     return 0;
-    //TODO:
 }
 uint8_t cpu::ABX() {
-    return 0;
-    //TODO:
+    //operand is address; effective address is address incremented by X **with** carry
+    //an additional clock cycle may be required if content changes
+    uint16_t lo = cpu_bus_read(pc);
+	pc++;
+	uint16_t hi = cpu_bus_read(pc);
+	pc++;
+
+	addr_abs = (hi << 8) | lo;
+	addr_abs += x_reg;
+
+	if ((addr_abs & 0xFF00) != (hi << 8))
+		return 1;
+	else
+		return 0;
 }
 uint8_t cpu::ABY() {
-    return 0;
-    //TODO:
+    uint16_t lo = cpu_bus_read(pc);
+	pc++;
+	uint16_t hi = cpu_bus_read(pc);
+	pc++;
+
+	addr_abs = (hi << 8) | lo;
+	addr_abs += y_reg;
+
+	if ((addr_abs & 0xFF00) != (hi << 8))
+		return 1;
+	else
+		return 0;
 }
 uint8_t cpu::IND() {
-    return 0;
-    //TODO:
+    //indirect
+	uint16_t ptr_lo = cpu_bus_read(pc);
+	pc++;
+	uint16_t ptr_hi = cpu_bus_read(pc);
+	pc++;
+
+	uint16_t ptr = (ptr_hi << 8) | ptr_lo;
+
+    //hardware bug
+	if (ptr_lo == 0x00FF) { 
+		addr_abs = (cpu_bus_read(ptr & 0xFF00) << 8) | cpu_bus_read(ptr + 0);
+	}
+    //normal behavoir
+	else {
+		addr_abs = (cpu_bus_read(ptr + 1) << 8) | cpu_bus_read(ptr + 0);
+	}
+	
+	return 0;
 }
 uint8_t cpu::IZX() {
+    //indirect with x reg offset
+    uint16_t t = cpu_bus_read(pc);
+	pc++;
+
+	uint16_t lo = cpu_bus_read((uint16_t)(t + (uint16_t)x_reg) & 0x00FF);
+	uint16_t hi = cpu_bus_read((uint16_t)(t + (uint16_t)x_reg + 1) & 0x00FF);
+
+	addr_abs = (hi << 8) | lo;
     return 0;
-    //TODO:
 }
 uint8_t cpu::IZY() {
-    return 0;
-    //TODO:
+    //indirect with x reg offset
+    //addititional clock cycle required upon content change
+    uint16_t t = cpu_bus_read(pc);
+	pc++;
+
+	uint16_t lo = cpu_bus_read(t & 0x00FF);
+	uint16_t hi = cpu_bus_read((t + 1) & 0x00FF);
+
+	addr_abs = (hi << 8) | lo;
+	addr_abs += y_reg;
+	
+	if ((addr_abs & 0xFF00) != (hi << 8))
+		return 1;
+	else
+		return 0;
 }
 
-//------------------------------INSTRUCTIONS-------------------------------//
-uint8_t cpu::ADC(){
-    return 0;
-    //TODO:
+//----------------------------------------INSTRUCTIONS----------------------------------------//
+uint8_t cpu::ADC() {
+    //add with carry
+    // Grab the data that we are adding to the accumulator
+	fetch();
+	
+	// Add is performed in 16-bit domain for emulation to capture any
+	// carry bit, which will exist in bit 8 of the 16-bit word
+	temp = (uint16_t)accu + (uint16_t)fetched + (uint16_t)get_flag(Carry);
+	
+	// The carry flag out exists in the high byte bit 0
+	set_flag(Carry, temp > 255);
+	
+	// The Zero flag is set if the result is 0
+	set_flag(Zero, (temp & 0x00FF) == 0);
+	
+	// The signed Overflow flag is set based on all that up there! :D
+	set_flag(Overflow, (~((uint16_t)accu ^ (uint16_t)fetched) & ((uint16_t)accu ^ (uint16_t)temp)) & 0x0080);
+	
+	// The negative flag is set to the most significant bit of the result
+	set_flag(Negative, temp & 0x80);
+	
+	// Load the result into the accumulator (it's 8-bit dont forget!)
+	accu = temp & 0x00FF;
+	
+	// This instruction has the potential to require an additional clock cycle
+	return 1;
 }
-uint8_t cpu::AND(){
-    return 0;
-    //TODO:
+uint8_t cpu::AND() {
+    //LOGICAL AND on accumulator and fetched data
+    fetch();
+	accu = accu & fetched;
+	set_flag(Zero, accu == 0x00);
+	set_flag(Negative, accu & 0x80);
+	return 1;
 }
-uint8_t cpu::ASL(){
-    return 0;
-    //TODO:
+uint8_t cpu::ASL() {
+    //bit shift left
+    fetch();
+	temp = (uint16_t)fetched << 1;
+	set_flag(Carry, (temp & 0xFF00) > 0);
+	set_flag(Zero, (temp & 0x00FF) == 0x00);
+	set_flag(Negative, temp & 0x80);
+	if (curr_inst->addr_mode == IMPX)
+		accu = temp & 0x00FF;
+	else
+		cpu_bus_write(addr_abs, temp & 0x00FF);
+	return 0;
 }
-uint8_t cpu::BCC(){
+uint8_t cpu::BCC() {
+    //branch if carry clear
+    if(get_flag(Carry) == 0) {
+        cycles++;
+		addr_abs = pc + addr_rel;
+
+		if ((addr_abs & 0xFF00) != (pc & 0xFF00))
+			cycles++;
+
+		pc = addr_abs;
+    }
     return 0;
-    //TODO:
 }
-uint8_t cpu::BCS(){
+uint8_t cpu::BCS() {
+    //branch if carry bit is set
+    if(get_flag(Carry) == 1) {
+        cycles++;
+		addr_abs = pc + addr_rel;
+
+		if ((addr_abs & 0xFF00) != (pc & 0xFF00))
+			cycles++;
+
+		pc = addr_abs;
+    }
     return 0;
-    //TODO:
 }
-uint8_t cpu::BEQ(){
+uint8_t cpu::BEQ() {
+    //branch if equal
+    if (get_flag(Zero) == 1)
+	{
+		cycles++;
+		addr_abs = pc + addr_rel;
+
+		if ((addr_abs & 0xFF00) != (pc & 0xFF00))
+			cycles++;
+
+		pc = addr_abs;
+	}
     return 0;
-    //TODO:
 }
-uint8_t cpu::BIT(){
+uint8_t cpu::BIT() {
+    fetch();
+	temp = accu & fetched;
+	set_flag(Zero, (temp & 0x00FF) == 0x00);
+	set_flag(Negative, fetched & (1 << 7));
+	set_flag(Overflow, fetched & (1 << 6));
     return 0;
-    //TODO:
 }
-uint8_t cpu::BMI(){
+uint8_t cpu::BMI() {
+    //branch if negative
+    if (get_flag(Negative) == 1)
+	{
+		cycles++;
+		addr_abs = pc + addr_rel;
+
+		if ((addr_abs & 0xFF00) != (pc & 0xFF00))
+			cycles++;
+
+		pc = addr_abs;
+	}
     return 0;
-    //TODO:
 }
-uint8_t cpu::BNE(){
+uint8_t cpu::BNE() {
+    //branch if not equal
+    if (get_flag(Zero) == 0)
+	{
+		cycles++;
+		addr_abs = pc + addr_rel;
+
+		if ((addr_abs & 0xFF00) != (pc & 0xFF00))
+			cycles++;
+
+		pc = addr_abs;
+	}
     return 0;
-    //TODO:
 }
-uint8_t cpu::BPL(){
+uint8_t cpu::BPL() {
+    //branch if positive
+    if (get_flag(Negative) == 0)
+	{
+		cycles++;
+		addr_abs = pc + addr_rel;
+
+		if ((addr_abs & 0xFF00) != (pc & 0xFF00))
+			cycles++;
+
+		pc = addr_abs;
+	}
     return 0;
-    //TODO:
 }
-uint8_t cpu::BRK(){
+uint8_t cpu::BRK() {
+    //break
+    pc++;
+	
+	set_flag(Disable_Interrupts, 1);
+	cpu_bus_write(0x0100 + sp, (pc >> 8) & 0x00FF);
+	sp--;
+	cpu_bus_write(0x0100 + sp, pc & 0x00FF);
+	sp--;
+
+	set_flag(Break, 1);
+	cpu_bus_write(0x0100 + sp, status);
+	sp--;
+	set_flag(Break, 0);
+
+	pc = (uint16_t)cpu_bus_read(0xFFFE) | ((uint16_t)cpu_bus_read(0xFFFF) << 8);
     return 0;
-    //TODO:
 }
-uint8_t cpu::BVC(){
+uint8_t cpu::BVC() {
+    //branch if overflow clear
+    if (get_flag(Overflow) == 0)
+	{
+		cycles++;
+		addr_abs = pc + addr_rel;
+
+		if ((addr_abs & 0xFF00) != (pc & 0xFF00))
+			cycles++;
+
+		pc = addr_abs;
+	}
     return 0;
-    //TODO:
 }
-uint8_t cpu::BVS(){
+uint8_t cpu::BVS() {
+    //branch if overflow set
+    if (get_flag(Overflow) == 1)
+	{
+		cycles++;
+		addr_abs = pc + addr_rel;
+
+		if ((addr_abs & 0xFF00) != (pc & 0xFF00))
+			cycles++;
+
+		pc = addr_abs;
+	}
     return 0;
-    //TODO:
 }
-uint8_t cpu::CLC(){
+uint8_t cpu::CLC() {
+    //clears the carry flag
+    set_flag(Carry, false);
     return 0;
-    //TODO:
 }
-uint8_t cpu::CLD(){
+uint8_t cpu::CLD() {
+    //clears the decimal flag
+    set_flag(Decimal, false);
     return 0;
-    //TODO:
 }
-uint8_t cpu::CLI(){
+uint8_t cpu::CLI() {
+    //clears interrupt flag
+    set_flag(Disable_Interrupts, false);
     return 0;
-    //TODO:
 }
-uint8_t cpu::CLV(){
+uint8_t cpu::CLV() {
+    //clears overflow flag
+    set_flag(Overflow, false);
     return 0;
-    //TODO:
 }
-uint8_t cpu::CMP(){
-    return 0;
-    //TODO:
+uint8_t cpu::CMP() {
+    // Instruction: Compare Accumulator
+    // Function:    C <- A >= M      Z <- (A - M) == 0
+    // Flags Out:   N, C, Z
+    fetch();
+	temp = (uint16_t)accu - (uint16_t)fetched;
+	set_flag(Carry, accu >= fetched);
+	set_flag(Zero, (temp & 0x00FF) == 0x0000);
+	set_flag(Negative, temp & 0x0080);
+    return 1;
 }
-uint8_t cpu::CPX(){
-    return 0;
-    //TODO:
+uint8_t cpu::CPX() {
+    //compare X register
+    fetch();
+	temp = (uint16_t)x_reg - (uint16_t)fetched;
+	set_flag(Carry, x_reg >= fetched);
+	set_flag(Zero, (temp & 0x00FF) == 0x0000);
+	set_flag(Negative, temp & 0x0080);
+	return 0;
 }
-uint8_t cpu::CPY(){
+uint8_t cpu::CPY() {
+    //compare Y register
+    fetch();
+	temp = (uint16_t)y_reg - (uint16_t)fetched;
+	set_flag(Carry, y_reg >= fetched);
+	set_flag(Zero, (temp & 0x00FF) == 0x0000);
+	set_flag(Negative, temp & 0x0080);
     return 0;
-    //TODO:
 }
-uint8_t cpu::DEC(){
+uint8_t cpu::DEC() {
+    //decrement value at memory location
+    fetch();
+	temp = fetched - 1;
+	cpu_bus_write(addr_abs, temp & 0x00FF);
+	set_flag(Zero, (temp & 0x00FF) == 0x0000);
+	set_flag(Negative, temp & 0x0080);
     return 0;
-    //TODO:
 }
-uint8_t cpu::DEX(){
+uint8_t cpu::DEX() {
+    //decrement x register
+    x_reg--;
+	set_flag(Zero, x_reg == 0x00);
+	set_flag(Negative, x_reg & 0x80);
     return 0;
-    //TODO:
 }
-uint8_t cpu::DEY(){
+uint8_t cpu::DEY() {
+    y_reg--;
+	set_flag(Zero, y_reg == 0x00);
+	set_flag(Negative, y_reg & 0x80);
     return 0;
-    //TODO:
 }
-uint8_t cpu::EOR(){
-    return 0;
-    //TODO:
+uint8_t cpu::EOR() {
+    //bitwise logic XOR
+    fetch();
+	accu = accu ^ fetched;	
+	set_flag(Zero, accu == 0x00);
+	set_flag(Negative, accu & 0x80);
+	return 1;
 }
-uint8_t cpu::INC(){
+uint8_t cpu::INC() {
+    //Increment data in memory
+    fetch();
+	temp = fetched + 1;
+	cpu_bus_write(addr_abs, temp & 0x00FF);
+	set_flag(Zero, (temp & 0x00FF) == 0x0000);
+	set_flag(Negative, temp & 0x0080);
     return 0;
-    //TODO:
 }
-uint8_t cpu::INX(){
+uint8_t cpu::INX() {
+    //increment x reg
+    x_reg++;
+	set_flag(Zero, x_reg == 0x00);
+	set_flag(Negative, x_reg & 0x80);
     return 0;
-    //TODO:
 }
-uint8_t cpu::INY(){
+uint8_t cpu::INY() {
+    //increment y_reg
+    y_reg++;
+	set_flag(Zero, y_reg == 0x00);
+	set_flag(Negative, y_reg & 0x80);
     return 0;
-    //TODO:
 }
-uint8_t cpu::JMP(){
+uint8_t cpu::JMP() {
+    //jump to location
+    pc = addr_abs;
     return 0;
-    //TODO:
 }
 uint8_t cpu::JSR(){
     return 0;
@@ -694,29 +960,57 @@ uint8_t cpu::LSR(){
     return 0;
     //TODO:
 }
-uint8_t cpu::NOP(){
-    return 0;
-    //TODO:
+uint8_t cpu::NOP() {
+    // Sadly not all NOPs are equal, added a few here
+	// based on https://wiki.nesdev.com/w/index.php/CPU_unofficial_opcodes
+	switch (opcode) {
+	case 0x1C:
+	case 0x3C:
+	case 0x5C:
+	case 0x7C:
+	case 0xDC:
+	case 0xFC:
+		return 1;
+		break;
+	}
+	return 0;
 }
-uint8_t cpu::ORA(){
-    return 0;
-    //TODO:
+uint8_t cpu::ORA() {
+    //logic OR
+    fetch();
+	accu = accu | fetched;
+	set_flag(Zero, accu == 0x00);
+	set_flag(Negative, accu & 0x80);
+	return 1;
 }
-uint8_t cpu::PHA(){
+uint8_t cpu::PHA() {
+    //pushes accumulator to the stack
+    cpu_bus_write(0x0100 + sp, accu);
+	sp--;
     return 0;
-    //TODO:
 }
-uint8_t cpu::PHP(){
+uint8_t cpu::PHP() {
+    //push status register to stack
+    cpu_bus_write(0x0100 + sp, status | Break | Unused);
+	set_flag(Break, 0);
+	set_flag(Unused, 0);
+	sp--;
     return 0;
-    //TODO:
 }
-uint8_t cpu::PLA(){
+uint8_t cpu::PLA() {
+    //POP accumulator off stack
+    sp++;
+	accu = cpu_bus_read(0x0100 + sp);
+	set_flag(Zero, accu == 0x00);
+	set_flag(Negative, accu & 0x80);
     return 0;
-    //TODO:
 }
-uint8_t cpu::PLP(){
-    return 0;
-    //TODO:
+uint8_t cpu::PLP() {
+    //pop status register off stack
+    sp++;
+	status = cpu_bus_read(0x0100 + sp);
+	set_flag(Unused, 1);
+	return 0;
 }
 uint8_t cpu::ROL(){
     return 0;
@@ -734,21 +1028,37 @@ uint8_t cpu::RTS(){
     return 0;
     //TODO:
 }
-uint8_t cpu::SBC(){
-    return 0;
-    //TODO:
+uint8_t cpu::SBC() {
+    fetch();
+	
+	// Operating in 16-bit domain to capture carry out
+	
+	// We can invert the bottom 8 bits with bitwise xor
+	uint16_t value = ((uint16_t)fetched) ^ 0x00FF;
+	
+	// Notice this is exactly the same as addition from here!
+	temp = (uint16_t)accu + value + (uint16_t)get_flag(Carry);
+	set_flag(Carry, temp & 0xFF00);
+	set_flag(Zero, ((temp & 0x00FF) == 0));
+	set_flag(Overflow, (temp ^ (uint16_t)accu) & (temp ^ value) & 0x0080);
+	set_flag(Negative, temp & 0x0080);
+	accu = temp & 0x00FF;
+	return 1;
 }
-uint8_t cpu::SEC(){
+uint8_t cpu::SEC() {
+    //sets carry flag
+    set_flag(Carry, true);
     return 0;
-    //TODO:
 }
-uint8_t cpu::SED(){
+uint8_t cpu::SED() {
+    //sets decimal flag
+    set_flag(Decimal, true);
     return 0;
-    //TODO:
 }
-uint8_t cpu::SEI(){
+uint8_t cpu::SEI() {
+    //sets interrupt flag
+    set_flag(Disable_Interrupts, true);
     return 0;
-    //TODO:
 }
 uint8_t cpu::STA(){
     return 0;
@@ -786,9 +1096,20 @@ uint8_t cpu::TYA(){
     return 0;
     //TODO:
 }
-uint8_t cpu::NONE(){
+uint8_t cpu::NONE() {
     return 0;
-    //TODO:
+}
+
+uint8_t cpu::fetch()
+{
+	if (!(curr_inst->addr_mode == IMPX))
+		fetched = cpu_bus_read(addr_abs);
+	return fetched;
+}
+
+bool cpu::complete()
+{
+	return cycles == 0;
 }
 
 void cpu::set_flag(cpu_flags flags, bool v) {
